c737fad8122491ec6621afb2167733682fc1bc96
Natthawut209 <std.66122420209@ubru.ac.th>
2026-03-23 16:05:25 +0700

    change sqlite to postgresdb

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 ﻿import os
-
 from flask import Flask
 
 from booking import db, login_manager, migrate
@@ -10,18 +9,23 @@ from booking.routes import main
 def create_app():
     base_dir = os.path.abspath(os.path.dirname(__file__))
     template_dir = os.path.join(base_dir, "booking", "templates")
+
     app = Flask(__name__, template_folder=template_dir)
 
-    db_path = os.path.join(base_dir, "room_booking.db").replace("\\", "/")
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "kwekxkkckakkseow!eooxcdada")
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+    # 🔥 ใช้ PostgreSQL จาก Render เท่านั้น
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if not DATABASE_URL:
+        raise ValueError("DATABASE_URL not set")
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "secret123")
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
 
-    main.template_folder = "templates"
     app.register_blueprint(main)
 
     @app.shell_context_processor
@@ -38,6 +42,9 @@ def create_app():
 
 app = create_app()
 
+# 🔥 สร้าง DB อัตโนมัติ (ไม่ต้องใช้ shell)
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
